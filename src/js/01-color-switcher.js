86461039819353af56420a212b50b9dee21b4b49
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-12 23:54:15 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+
+const body = document.body;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intId = null;
+let isActive = false;
+
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+    
+
+function onStartClick() {
+    if (isActive) {
+        return
+    }
+    isActive = true;
+    intId = setInterval(() => { body.style.backgroundColor = getRandomHexColor(); }, 1000 )
+}
+
+function onStopClick() {
+    clearInterval(intId);
+    isActive = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
