7162cd17fc81be0d9839029378b2f280e7f06d6f
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-15 02:10:33 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,24 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 
 Notify.init({
     position: 'center-top',
 });
 
+
 const todayDate = new Date();
+
+let selectedDate = 0;
+
+const btnStart = document.querySelector("[data-start]")
+const day = document.querySelector("[data-days]")
+const hour = document.querySelector("[data-hours]")
+const minute = document.querySelector("[data-minutes]")
+const second = document.querySelector("[data-seconds]")
+const inputFlatpickr = document.querySelector("#datetime-picker")
+
 
 const options = {
     enableTime: true,
@@ -17,46 +29,64 @@ const options = {
         // console.log(selectedDates[0]);
         selectedDatechecker(selectedDates);
     },
-    
+
 };
-let selectedDate = 0;
-
-const btnStart = document.querySelector("[data-start]")
-const day = document.querySelector("[data-days]")
-const hour = document.querySelector("[data-hours]")
-const minute = document.querySelector("[data-minutes]")
-const second = document.querySelector("[data-seconds]")
-
 flatpickr("#datetime-picker", options);
 
 
 btnStart.addEventListener('click', onStartClick);
 
 
-
-// position center - center
 function onStartClick() {
     let dayToEnd = 0;
+    inputFlatpickr.disabled = true;
     const IdInt = setInterval(
         () => {
+           
             dayToEnd = selectedDate - Date.now();
+           
+            if (dayToEnd > 0) {
+                sorterOfValuesByMarkup(dayToEnd);
+            } else {
+                clearInterval(IdInt);
+                Confirm.show(
+                    'Countdown completed successfully!',
+                    'Shall we start again??',
+                    'Yes',
+                    'No',
+                    () => {
+                        document.location.reload();
+                    },
+                    () => {
+                        Notify.warning('If you say no... you can stay here and do nothing.');
+                        // alert('If you say no... you can stay here and do nothing.');
+                    },
+                    {
+                    },
+                );
+            }
             
-            const { days, hours, minutes, seconds } = convertMs(dayToEnd);
-
-            day.textContent = addLeadingZero(days);
-            hour.textContent = addLeadingZero(hours);
-            minute.textContent = addLeadingZero(minutes);
-            second.textContent = addLeadingZero(seconds);
 
         }, 1000); 
+
     Notify.success('It&#39;s the final countdown!!!');
            
 };
+
+function sorterOfValuesByMarkup(dayToEnd) {
+    const { days, hours, minutes, seconds } = convertMs(dayToEnd);
+
+    day.textContent = addLeadingZero(days);
+    hour.textContent = addLeadingZero(hours);
+    minute.textContent = addLeadingZero(minutes);
+    second.textContent = addLeadingZero(seconds);
+}
 
 function selectedDatechecker(selectedDates) {
     if ((selectedDates[0].getTime() - todayDate.getTime()) < 0) {
         Notify.failure('Please choose a date in the future');
         // alert("Please choose a date in the future");
+        btnStart.disabled = true;
         return;
     }
     btnStart.disabled = false;
